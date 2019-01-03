c977a16ea02a0c4e23819591d4c65da7aafaca4c
Lukasz1928 <Lukasz19281@gmail.com>
2019-01-03 18:36:23 +0100

    Added all leaflets types

---
+++ src/main/java/heart/valve/leaflets/leaflets/MitralValveLeaflets.java
@@ -0,0 +1,7 @@
+package heart.valve.leaflets.leaflets;
+
+import heart.valve.leaflets.LeafletsType;
+
+public enum MitralValveLeaflets implements LeafletsType {
+    RIGHT, FIBROTIC, CALCIFIED, FRONT_PROLAPS, BACK_PROLAPS, BOTH_PROLAPS, RING_CALCIFIED
+}
---
+++ src/main/java/heart/valve/leaflets/leaflets/TriscupidValveLeaflets.java
@@ -0,0 +1,5 @@
+package heart.valve.leaflets.leaflets;
+
+public enum TriscupidValveLeaflets {
+    RIGHT, FIBROTIC, CALCIFIED
+}
