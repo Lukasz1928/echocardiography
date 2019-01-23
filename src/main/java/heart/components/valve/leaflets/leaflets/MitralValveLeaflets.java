5e4aeac99e19f73e5c70ba0a38a57b3d7f13d8e3
Lukasz1928 <Lukasz19281@gmail.com>
2019-01-23 12:28:30 +0100

    Changed Leaflets fields order

--- src/main/java/heart/components/valve/leaflets/leaflets/MitralValveLeaflets.java
+++ src/main/java/heart/components/valve/leaflets/leaflets/MitralValveLeaflets.java
@@ -3,5 +3,5 @@ package heart.components.valve.leaflets.leaflets;
 import heart.components.valve.leaflets.LeafletsType;
 
 public enum MitralValveLeaflets implements LeafletsType {
-    RIGHT, FIBROTIC, CALCIFIED, FIBROTIC_WITH_CALCIFICATIONS, FRONT_PROLAPS, BACK_PROLAPS, BOTH_PROLAPS, CALCIFIED_RING
+    RIGHT, FIBROTIC, CALCIFIED, FIBROTIC_WITH_CALCIFICATIONS, BOTH_PROLAPS, BACK_PROLAPS, FRONT_PROLAPS, CALCIFIED_RING
 }
