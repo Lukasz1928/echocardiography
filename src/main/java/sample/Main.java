9557ea4bb63fa341ef042e9fac97b75bdc47efff
Lukasz1928 <Lukasz19281@gmail.com>
2019-01-30 22:38:06 +0100

    Short-term changes in Main only for debugging purposes

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,25 +1,41 @@
 package sample;
 
+import gui.utils.LatexRenderer;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class Main extends Application {
+public class Main {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        ResourceBundle bundle = ResourceBundle.getBundle("gui.text.guiLabels", new Locale("pl", "PL"));
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/templates/mainWindow.fxml"), bundle);
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//        ResourceBundle bundle = ResourceBundle.getBundle("gui.text.guiLabels", new Locale("pl", "PL"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/gui/templates/mainWindow.fxml"), bundle);
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root));
+//        primaryStage.show();
+//    }
 
     public static void main(String[] args) {
-        launch(args);
+
+        LatexRenderer lr = new LatexRenderer();
+
+        File outputfile = new File("aaa_bbb_ccc.png");
+        BufferedImage image = SwingFXUtils.fromFXImage(lr.latexToImage("{aaa}^{bbb}^{bbb}^{ddd}^{ggg}", 15), null);
+        try {
+            ImageIO.write(image, "png", outputfile);
+        }
+        catch(Exception e) {
+            //
+        }
+        //launch(args);
     }
 }
